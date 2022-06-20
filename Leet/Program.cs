ed87ca6708e1810ef6d415bd2396e0daeee65aa9
Matheus Reis <math.reis.dev@gmail.com>
2022-06-20 13:27:22 -0300

    13. Roman to Integer

--- Leet/Program.cs
+++ Leet/Program.cs
@@ -6,6 +6,100 @@ public class Solution
 
     }
 
+    //13. Roman to Integer: https://leetcode.com/problems/roman-to-integer/
+    public static int RomanToInt(string s)
+    {
+        int num = 0;
+        int initialLen = s.Length;
+
+        for (int i = 0; i < initialLen; i++)
+        {
+            int len = s.Length;
+
+            string lastChar = s.Length > 1 ? s.Substring(len - 1, 1) : s;
+
+            string lastTwoChar = s.Length > 2 ? s.Substring(len - 2, 2) : s;
+
+            if (lastTwoChar == "IV" || lastTwoChar == "IX" || lastTwoChar == "XL" || 
+                lastTwoChar == "XC" || lastTwoChar == "CD" || lastTwoChar == "CM")
+            {
+                switch (lastTwoChar)
+                {
+                    case "IV":
+                        s = s.Remove(len - 2, 2);
+                        num += 4;
+                        i++;
+                        break;
+                    case "IX":
+                        s = s.Remove(len - 2, 2);
+                        num += 9;
+                        i++;
+                        break;
+                    case "XL":
+                        s = s.Remove(len - 2, 2);
+                        num += 40;
+                        i++;
+                        break;
+                    case "XC":
+                        s = s.Remove(len - 2, 2);
+                        num += 90;
+                        i++;
+                        break;
+                    case "CD":
+                        s = s.Remove(len - 2, 2);
+                        num += 400;
+                        i++;
+                        break;
+                    case "CM":
+                        s = s.Remove(len - 2, 2);
+                        num += 900;
+                        i++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                switch (lastChar)
+                {
+                    case "I":
+                        s = s.Remove(len - 1, 1);
+                        num++;
+                        break;
+                    case "V":
+                        s = s.Remove(len - 1, 1);
+                        num += 5;
+                        break;
+                    case "X":
+                        s = s.Remove(len - 1, 1);
+                        num += 10;
+                        break;
+                    case "L":
+                        s = s.Remove(len - 1, 1);
+                        num += 50;
+                        break;
+                    case "C":
+                        s = s.Remove(len - 1, 1);
+                        num += 100;
+                        break;
+                    case "D":
+                        s = s.Remove(len - 1, 1);
+                        num += 500;
+                        break;
+                    case "M":
+                        s = s.Remove(len - 1, 1);
+                        num += 1000;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        return num;
+    }
+
     //9. Palindrome Number: https://leetcode.com/problems/palindrome-number/
     public static bool IsPalindrome(int x)
     {
